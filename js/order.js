79abb3054c5666e258548f084a6aba486fe70796
nadaOsama20 <101597758+nadaOsama20@users.noreply.github.com>
2025-08-01 13:15:52 +0300

    Add files via upload

---
+++ js/order.js
@@ -0,0 +1,37 @@
+const Orders = [{
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'LADVENDER KF102 Drone',
+        productNumber: '36557',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '65214',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera Done',
+        productNumber: '58743',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'GPS 4k Drane',
+        productNumber: '48515',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Lozende Drone',
+        productNumber: '25232',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+]
